05e8c5b513f4023e93ae56b94cfec332b1739fc3
panbin <1764183867@qq.com>
2023-05-23 12:17:39 +0800

    Add Startup Program

---
+++ app_manager_startup/Main.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace app_manager_startup
+{
+	partial class Main
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			StartUp = new Button();
+			HostTextBox = new TextBox();
+			SuspendLayout();
+			// 
+			// StartUp
+			// 
+			StartUp.Location = new Point(102, 86);
+			StartUp.Name = "StartUp";
+			StartUp.Size = new Size(171, 49);
+			StartUp.TabIndex = 0;
+			StartUp.Text = "启动分发平台";
+			StartUp.UseVisualStyleBackColor = true;
+			StartUp.Click += StartUp_Click;
+			// 
+			// HostTextBox
+			// 
+			HostTextBox.Location = new Point(59, 22);
+			HostTextBox.Name = "HostTextBox";
+			HostTextBox.Size = new Size(253, 23);
+			HostTextBox.TabIndex = 1;
+			// 
+			// Main
+			// 
+			AutoScaleDimensions = new SizeF(7F, 17F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(379, 170);
+			Controls.Add(HostTextBox);
+			Controls.Add(StartUp);
+			Name = "Main";
+			Text = "AppManager";
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Button StartUp;
+		private TextBox HostTextBox;
+	}
+}
---
+++ app_manager_startup/Main.cs
@@ -0,0 +1,58 @@
+using System.Diagnostics;
+using System.Security.AccessControl;
+using System.Security.Policy;
+using System.Text.RegularExpressions;
+
+namespace app_manager_startup
+{
+	public partial class Main : Form
+	{
+		public static FTPServer FTPServer;
+		public Main()
+		{
+			InitializeComponent();
+			InitLocalHost();
+		}
+
+		private void InitLocalHost()
+		{
+			string host = "https://" + GetIP() + ":5000";
+			HostTextBox.Text = host;
+		}
+
+		private void StartUp_Click(object sender, EventArgs e)
+		{
+			
+			//if (FTPServer == null)
+			//{
+			//	FTPServer = new FTPServer("Program\\wwwroot\\files", "FTPServer\\hfs.exe", "FTPServer\\config.yaml");
+			//}
+			//else
+			//{
+			//	return;
+			//}
+
+			Process cmd = new Process();
+
+			cmd.StartInfo.FileName = "Program\\SampleAppManager.exe";
+			cmd.StartInfo.Arguments = "--urls " + HostTextBox.Text;
+			cmd.Start();
+		}
+
+		private string GetIP()
+		{
+			try
+			{
+				System.Net.Sockets.TcpClient c = new System.Net.Sockets.TcpClient();
+				c.Connect("www.baidu.com", 80);
+				string ip = ((System.Net.IPEndPoint)c.Client.LocalEndPoint).Address.MapToIPv4().ToString();
+				c.Close();
+				return ip;
+			}
+			catch (Exception)
+			{
+				return null;
+			}
+		}
+	}
+}
